ec7d683d609e70228cabebabf6dbeeb5a73b27d5
giovannicarmo <giovannicarmo96@gmail.com>
2018-04-16 20:55:27 -0300

    Habiliyacao do post

--- src/main/java/com/giovannicarmo/webserviceappoio/config/SecurityConfig.java
+++ src/main/java/com/giovannicarmo/webserviceappoio/config/SecurityConfig.java
@@ -39,6 +39,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     };
 
     private static final String[] PUBLIC_MATCHERS_GET = {
+        "/usuarios/**"
+    };
+    private static final String[] PUBLIC_MATCHERS_POST = {
             "/usuarios/**"
     };
 
@@ -51,6 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.cors().and().csrf().disable();
         http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_POST).permitAll()
                 .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
