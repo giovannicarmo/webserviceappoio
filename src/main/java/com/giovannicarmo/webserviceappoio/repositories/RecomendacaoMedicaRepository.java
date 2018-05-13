6eaa8d29d3119020276073bc70da173c1052e58e
giovannicarmo <giovannicarmo96@gmail.com>
2018-05-13 17:48:31 -0300

    Aequivos lixo

--- src/main/java/com/giovannicarmo/webserviceappoio/repositories/RecomendacaoMedicaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.giovannicarmo.webserviceappoio.repositories;
-
-import com.giovannicarmo.webserviceappoio.domain.RecomendacaoMedica;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecomendacaoMedicaRepository extends JpaRepository<RecomendacaoMedica, Integer> {
-}
